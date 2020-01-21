de0e0e39cafba3f8b602566df180b0638cfbcb5c
Sam <sam@micous.com>
2020-01-21 16:35:46 +0800

    chapter3

---
+++ teach-yourself-v7/chapter03/js/test001.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Sam on 2020/1/21.
+ */
+function doThis() {
+    alert('Doing This');
+}
+
+function doThat() {
+    alert('Doing That');
+}
+
+function sayHello() {
+    alert('Hello');
+}
